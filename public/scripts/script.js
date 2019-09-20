44bae8c2bad2f07f7ff6a831f5f9ecd7474d181f
Gustavo Magnago <gusmagnago@gmail.com>
2019-09-20 12:20:37 +0100

    to pull

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', () => {
   console.log('Ironmaker app started successfully!');
 }, false);
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".navbar-fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
